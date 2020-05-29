133a6284586281dcbbcae7b08370df6aa41e4566
23apiel <63662407+23apiel@users.noreply.github.com>
2020-05-29 00:54:51 -0700

    Update focus_day_project.py

--- focus_day_project.py
+++ focus_day_project.py
@@ -1,6 +1,21 @@
-# Welcome to your Focus Day Project. Replace this comment with something that introduces the user to your project. Be sure to mention the Focus Day and your initials and graduation year. (ie This game is for Pool Volume Day and is written by ML '23.)
-# Also, be sure to use comments throughout your program. Use good programming practices, including organization, documentation and citation. Yes, you need to cite your sources! (You can do so using comments at the bottom of your code.)
+# Project Description
 
+#Focus Day: Harry Potter Day
+
+#Written by: Annabel Piel 
+
+#Graduating Year: 2023
+
+#Add info about game here: I have created a Jeopardy game for Harry Potter Day this will test how well you know the Harry Potter universe:
+#1.	Split the group into 2 teams.
+#2.	Have one group answer the 1st question, “Are you group 1 or 2?”
+#3.	Then you will be asked to select a category you have 3 options: Hogwarts, Geography, and Random Facts
+#4.	After you will be asked to select an amount of points: 100, 200, 300, 400, and 500.
+#5.	Then answer the question that corresponds to the number you picked.
+#6.	Repeat the last 3 steps for group two, then repeat the last 3 steps for group 1, and ect.  
+#7.	Once you have answered a question both teams can’t answer the question again. 
+#8.	At the end of the game look at the 2 final scores and determine the winner you will only be able to play for 20 rounds.
+# *you must also use correct spelling and capitalization for the code to work
 
 
 # this function conains 5 questions about Hogwarts for group 1
